8e62216be375392d579438e65d71016dfd6de4a2
Popescu Andrei <popescuandrei@Popescus-iMac.local>
2024-08-09 16:09:53 +0300

    Renamed commit

--- plugins/modules/server.py
+++ plugins/modules/server.py
@@ -494,7 +494,8 @@ def main():
             ("state", "present", ["image"]),
             ("state", "present", ["package"]),
         ],
-        required_one_of=["password", "ssh_pub_key"]
+        required_one_of=["password", "ssh_pub_key"],
+        required_by="auto",
     )
     PidginHostCloud(module)
 
